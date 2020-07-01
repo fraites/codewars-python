c514e8cef1ce7c7747d3a7a7f391ec769906f613
fraites <59290923+fraites@users.noreply.github.com>
2020-06-30 23:02:46 -0400

    Update buying-car.py

--- buying-car.py
+++ buying-car.py
@@ -1,27 +1,9 @@
 def nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth):
-    count = 0
-    total_saved = 0
-    rate = 10**(-2)*percentLossByMonth*count
-    price_old = startPriceOld * (1 - (rate * count)) + total_saved
-    price_new = startPriceNew * (1 - (rate * count))
-    # this loop has the right idea, but doesnt work as planned
-    while (price_old + savingperMonth*count) < price_new:
-        total_saved = savingperMonth * count
-        if count % 2 == 0:
-            # successfully increases the rate by 0.5 every 2 months
-            rate += 0.5
-        test_price = price_old - (price_old * percentLossByMonth/100)
-        price_old -= (price_old * percentLossByMonth/100)
-        price_new -= (price_new * percentLossByMonth/100)
-        new_total = price_old + total_saved
-        print(price_new)
-        print(total_saved + price_old)
-        count += 1
-    # this only works for test case where old > new already
-    return [count,((price_old + total_saved) - price_new)]
-
-# custom function used to increase rate based on variable % per month
-def rate_increase(n, percentPerMonth):
-    rate = 10**(-2)*(percentPerMonth + (0.5*n))
-    return rate
-   
+    i = 0
+    while savingperMonth * i + startPriceOld < startPriceNew:
+        if i % 2:
+            percentLossByMonth += 0.5
+        startPriceOld -= startPriceOld * 0.01 * percentLossByMonth
+        startPriceNew -= startPriceNew * 0.01 * percentLossByMonth
+        i += 1
+    return [i, round(savingperMonth * i + startPriceOld - startPriceNew)]
