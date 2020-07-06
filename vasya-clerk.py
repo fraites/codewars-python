ea2b7b742a5c8119a214560f80c8367db7c6e60f
fraites <59290923+fraites@users.noreply.github.com>
2020-07-06 10:32:32 -0400

    Update vasya-clerk.py

--- vasya-clerk.py
+++ vasya-clerk.py
@@ -1,35 +1,22 @@
-# this function works for 18/20 cases
-# need to determine point for failures
+# took another approach to use $ type as key & quantity as value
 def tickets(people):
-    # array variable to use as register
-    register = []
-    # cost of ticket
-    cost = 25
-    # variable to hold change needed to return
-    change = 0
-    index_25 = 0
-    index_50 = 0
-    # go through each x in people
-    # add payment to new array to use as register
-    for x in people:
-        # add payment to register
-        register.append(x)
-        # determine if they paid more than cost
-        if x > cost:
-            change = x - cost
-            # determines how much change to give back
-            if change == 75:
-                if 25 not in register or 50 not in register:
-                    return "NO"
-                else:
-                    index_25 = register.index(25)
-                    index_50 = register.index(50)
-                    del register[index_25]
-                    del register[index_50]
+    # dict object to represent payment to cashier
+    register = {100:0,50:0,25:0}
+    for pay in people:
+        if pay == 25:
+            register[25] += 1
+        elif pay == 50:
+            if register[25] == 0:
+                return 'NO'
+            register[50] += 1
+            register[25] -= 1
+        else:
+            register[100] += 1
+            if register[50] >= 1 and register[25] >= 1:
+                register[50] -= 1
+                register[25] -= 1
+            elif register[25] >= 3:
+                register[25] -= 3
             else:
-                if 25 not in register:
-                    return "NO"
-                else:
-                    index_25 = register.index(25)
-                    del register[index_25]
-    return "YES"
+                return 'NO'
+    return 'YES'
