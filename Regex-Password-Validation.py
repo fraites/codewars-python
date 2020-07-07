f62e36d38abc053677b4e5155988d4175e664f4b
fraites <59290923+fraites@users.noreply.github.com>
2020-07-06 21:35:12 -0400

    Update Regex-Password-Validation.py

--- Regex-Password-Validation.py
+++ Regex-Password-Validation.py
@@ -1,1 +1,16 @@
-def search(regex, password):
+# Kata instructions
+# Password must be at least 6 characters
+# Password must contain upper case letter
+# Password must contain lower case letter
+# Password must contain a number
+# Password must be alphanumeric
+import re
+# the password will be automatically asserted by test cases. Below is only an example
+password = "4fdg5Fj3"
+# you only need to fill in the search condition you're looking for to complete this kata
+regex = re.compile("^(?=.*[a-z])(?=.*[a-z])(?=.*[\d])[a-zA-Z\d]{6,}$")
+if regex.search(password):
+    print(True)
+else:
+    print(False)
+
